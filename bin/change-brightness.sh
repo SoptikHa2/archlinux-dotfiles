90d9fa51611cf70dcb9a0d2f211afc176e77157d
Petr Stastny <petr.stastny01@gmail.com>
2019-03-21 18:32:39 +0000

    Moved .sh files into bin folder

---
+++ bin/change-brightness.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Assuming curent user uses inteL_backlight and has access to /sys/class/backlight/intel_backlight/{brightness,max_brightness}.
+# This can be set for even non-root users by adding rule:
+# < /etc/udev/rules.d/30-brightness.rules >
+# KERNEL=="intel_backlight", SUBSYSTEM=="backlight", RUN+="/usr/bin/find /sys/class/backlight/intel_backlight/ -type f -name brightness -exec chown petr:petr {} ; -exec chmod 666 {} ;"
+
+# Pass either 0 to decrease or 1 to increase brightness (as first argument)
+
+max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+new_brightness=0
+
+if [ $1 -eq 0 ]; then
+	new_brightness=$(($current_brightness - 100))
+fi
+if [ $1 -eq 1 ]; then
+	new_brightness=$(($current_brightness + 100))
+fi
+
+if [ $new_brightness -lt 5 ]; then
+	exit 1
+fi
+if [ $new_brightness -ge $max_brightness ]; then
+	exit 1
+fi
+
+echo $new_brightness > /sys/class/backlight/intel_backlight/brightness
