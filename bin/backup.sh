86711c661bb158d85150fc77a70bf8ee6de2bfe0
Petr Stastny <petr.stastny01@gmail.com>
2019-04-03 20:48:00 +0200

    Improved security of backup script
    Backup script tgz's folders, than encrypts them with GPG, uploads it via
    ftps and then deletes all the files.
    
    The problem is, the plaintext .tgz (not yet encrypted) file is
    accessible in /tmp until ftps upload finished. This is severe security
    risk, as there is pretty large attack window.
    
    Now, the plaintext .tgz is deleted as soon as it's encrypted with GPG.
    
    For even more privacy and security (which I as sinlge user of my
    computer don't need), change the /tmp to your home directory so no
    non-root user can access it anyway.

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,10 +6,17 @@
 filename_date=$(date +%Y-%m-%d-%H-%M)
 
 removetmp() {
-	shred -u -n 1 "/tmp/tmp-backupfile-"$filename_date".tgz"
+	# Shred plaintext file and remove encrypted file.
+	
+	# Unless something bad happens, the plaintext file should not exist
+	# when this is called. So we can just silence shred to not warn us the
+	# file doesn't exist
+	shred -u -n 1 "/tmp/tmp-backupfile-"$filename_date".tgz" 2>/dev/null
 	rm "/tmp/tmp-backupfile-"$filename_date".tgz.gpg"
 }
 trap removetmp HUP INT TERM
+
+echo "Archiving and compressing..."
 
 # Do not backup encrypted folder, as it's large, not updated often, and should be updated manually when changed
 # Don't backup AUR, Applications, Torrent, or Steam either
@@ -29,8 +36,11 @@ echo "WARNING: /Encrypted DIRECTORY NOT BACKED UP!"
 
 # Now encrypt it
 gpg --symmetric "/tmp/tmp-backupfile-"$filename_date".tgz"
-# And upload it via ftps (user will be asked for password)
-echo "Encrypted. Now, you'll be asked for FTPS password"
+echo "Encrypted. Deleting plaintext archive..."
+# Delete plaintext archive
+shred -u -n 1 "/tmp/tmp-backupfile-"$filename_date".tgz"
+# And upload the encrypted archive via ftps (user will be asked for password)
+echo "Deleted. Enter your FTPS password"
 lftp -u Petr -e "set ssl:verify-certificate false; cd home; cd backup; put /tmp/tmp-backupfile-"$filename_date".tgz.gpg; exit" mynas
 
 echo "Backup finished. Removing temporary files..."
