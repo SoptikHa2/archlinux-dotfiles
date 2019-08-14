e973596da9be98805451966165711f8db8bd64d9
Petr Šťastny <petr.stastny01@gmail.com>
2019-08-14 21:31:20 +0200

    Fixed polybar to appear on all monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars
-polybar top &
+# Set polybar to work on multiple monitors
+# Thanks, @apetresc! https://github.com/polybar/polybar/issues/763#issuecomment-331604987
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
 
 echo "Polybar launched"
