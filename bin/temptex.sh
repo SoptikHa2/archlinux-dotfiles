a80355762bc5008e08f893b27ac715a2b01581e2
Petr Šťastný <petr.stastny01@gmail.com>
2019-12-14 11:36:47 +0100

    Changed vim to nvim in temptex

--- bin/temptex.sh
+++ bin/temptex.sh
@@ -2,6 +2,6 @@
 # Create new directory and launch vim in it. Once vim exits, remove the directory.
 tempdir=$(mktemp -d) || exit 1
 
-vim "$tempdir/temptex.tex"
+nvim "$tempdir/temptex.tex"
 
 rm -rf "$tempdir"
