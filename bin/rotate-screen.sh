34061affd11cf1730e7e0c91562fed317f683494
Petr Šťastný <petr.stastny01@gmail.com>
2020-02-22 19:35:20 +0100

    Added screen rotation script
    This not only rotates screen, but touchpad as well.
    
    Source:
    https://askubuntu.com/questions/368317/rotate-touch-input-with-touchscreen-and-or-touchpad
    https://wiki.ubuntu.com/X/InputCoordinateTransformation

---
+++ bin/rotate-screen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script accepts one parameter: xrandr-like display orientation
+
+# This not only calls xrandr, but also changes laptop touchpad orientation for
+# easy usage
+# xinput set-prop "ELAN469D:00 04F3:304B Touchpad" --type=float "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
+target_screen="eDP-1-1"
+# You can find this out by using $(xinput)
+target_touchpad="ELAN469D:00 04F3:304B Touchpad"
+
+if [ -z "$1" ]; then
+	echo "Unknown orientation. Expected usage: $ rotate-screen.sh <left|right|normal|inverted>"
+	exit 1
+fi
+
+transformation_matrix=""
+if [ "$1" == "normal" ]; then
+	transformation_matrix="0 0 0 0 0 0 0 0 0"
+elif [ "$1" == "inverted" ]; then
+	transformation_matrix="-1 0 1 0 -1 1 0 0 1"
+elif [ "$1" == "left" ]; then
+	transformation_matrix="0 -1 1 1 0 0 0 0 1"
+elif [ "$1" == "right" ]; then
+	transformation_matrix="0 1 0 -1 0 1 0 0 1"
+else
+	echo "Uknown orientation. Expected usage: $ rotate-screen.sh <left|right|normal|inverted>"
+	exit 1
+fi
+
+xrandr --output "$target_screen" --rotation "$1"
+xinput set-prop "$target_touchpad" --type=float "Coordinate Transformation Matrix" $transformation_matrix
