515d6bc027b736d95415bdfa1018b38123e1bfdf
Petr Šťastný <petr.stastny01@gmail.com>
2020-03-26 16:08:05 +0100

    Added notification pausing while screen is locked

--- bin/lock-custom.sh
+++ bin/lock-custom.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+unpause_notifications() {
+  notify-send "DUNST_COMMAND_RESUME"
+}
+
+trap 'unpause_notifications' EXIT
+
+# Pause notifications
+notify-send "DUNST_COMMAND_PAUSE"
 # i3 lock with custom image
-i3lock -i "/home/petr/archlinux-dotfiles/lockscreen.png" -ue
+i3lock -i "/home/petr/archlinux-dotfiles/lockscreen.png" -ue &
+wait
+# Notifications will be unpaused when this script exits
