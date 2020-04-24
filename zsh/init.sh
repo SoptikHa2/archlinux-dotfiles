99ab706149e4f1c82f73684b6672836a07537668
Petr Šťastný <petr.stastny01@gmail.com>
2020-04-25 00:52:18 +0200

    Added syntax highlighting to zsh!

--- zsh/init.sh
+++ zsh/init.sh
@@ -2,3 +2,7 @@
 if command -v pazi &>/dev/null; then
   eval "$(pazi init zsh)" # or 'bash'
 fi
+
+# Enable syntax highlighting
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
