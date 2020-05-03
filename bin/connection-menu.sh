9176892f67a6d396f0d87b504df6f1a86cb215ff
Petr Šťastný <petr.stastny01@gmail.com>
2020-05-03 10:53:07 +0200

    Added bluetooth connect

--- bin/connection-menu.sh
+++ bin/connection-menu.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+#!/bin/zsh
 source "$HOME/archlinux-dotfiles/bin/secret"
 
 
-# Launch rofi. Either connection to wifi can be selected, to ethernet, or
+# Launch rofi. Either connection to wifi can be selected, to bluetooth, or
 # turn on Alza VPN.
 
 option_wifi=""
-option_ethernet=""
+option_bluetooth=""
 option_vpn=""
 
-chosen="$(echo -e "$option_wifi\n$option_ethernet\n$option_vpn" | rofi -theme "networkmenu.rasi" -dmenu)"
+chosen="$(echo -e "$option_wifi\n$option_bluetooth\n$option_vpn" | rofi -theme "networkmenu.rasi" -dmenu)"
 
 case $chosen in
 	$option_wifi)
         rofi-wifi-menu
 		;;
-	$option_ethernet)
-        # TODO
+	$option_bluetooth)
+        # Make sure bluetoothd is enabled
+        # https://www.linuxquestions.org/questions/programming-9/control-bluetoothctl-with-scripting-4175615328/
+        coproc bluetoothctl
+        echo -e 'power on\n connect 1C:52:16:CA:EE:DF\nexit\n' >&p
+        sleep 3
 		;;
 	$option_vpn)
         if [[ $(nmcli connection show --active | grep "Alza VPN" -c) -eq 0 ]]; then
