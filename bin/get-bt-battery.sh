63723c184ea421e7fa372d8519f583fa5db04ecb
Petr Šťastný <petr.stastny01@gmail.com>
2020-05-24 03:33:01 +0200

    Added script that checks BT battery

---
+++ bin/get-bt-battery.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+set -euo pipefail
+
+# https://github.com/TheWeirdDev/Bluetooth_Headset_Battery_Level
+percentage=$(/home/petr/data/Applications/Bluetooth_Headset_Battery_Level/bl_battery.py 1C:52:16:CA:EE:DF.1 | grep -o '[0-9]*%')
+
+echo ": $percentage"
