46056548170c48a6d38ba6d1cac3eb4d2779e87b
Petr Šťastný <petr.stastny01@gmail.com>
2020-06-01 06:54:27 +0200

    Added kexec to reboot in half the usual time

---
+++ bin/kexec.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This script restarts kernel. Useful if one needs to reboot but doesn't
+# actually want to reboot as it takes up to 7 seconds.
+# This script does the same in half the time.
+# This needs to be run as root.
+
+# Tell kexec what to boot
+kexec -l /boot/vmlinuz-linux --initrd=/boot/initramfs-linux.img --reuse-cmdline
+# Execute kexec. This will restart everything, including kernel.
+systemctl kexec
