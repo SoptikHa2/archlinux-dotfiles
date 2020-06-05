bc4263ea2f8a0d5cac4cc9b36d48e76f3e6a4c97
Petr Šťastný <petr.stastny01@gmail.com>
2020-06-06 00:08:14 +0200

    Modified enclip function to take advantage of rofi

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -9,6 +9,7 @@ cheatsh () {
 	# Credit: https://unix.stackexchange.com/questions/55546/removing-color-codes-from-output#55547
 }
 
+
 # Decrypt from clipboard
 declip () {
 xclip -o | gpg -d
@@ -16,5 +17,19 @@ xclip -o | gpg -d
 
 # Encrypt from "$1" for user "$2" and save to clipboard
 enclip () {
-echo "$1" | gpg --hidden-recipient "$2" --armor --encrypt - | xclip
+text="$1"
+recipient="$2"
+if [ -z "$text" ]; then
+    text=$(echo "Message to encrypt" | rofi -dmenu)
+    if [ -z "$text" ]; then return; fi
+fi
+if [ -z "$recipient" ]; then
+    recipient=$(gpg --list-public-keys | awk '
+BEGIN { FS="] " }
+/^pub/ { status=1 }
+/^ / { if (status == 1) { fingerprint=gensub(/ /, "", "g", $1); status=2 } }
+/^uid/ { if (status == 2) { status = 0; print fingerprint " " $2 } }' | rofi -dmenu -i | cut -d" " -f1)
+    if [ -z "$recipient" ]; then return; fi
+fi
+echo "$text" | gpg --recipient "$recipient" --armor --encrypt - | xclip
 }
