1f10b49a1f3adbe38855c45652c682e94f23908e
Petr Šťastný <petr.stastny01@gmail.com>
2020-06-11 12:30:50 +0200

    Added option to specify tesseract language

--- bin/copy-from-screen.sh
+++ bin/copy-from-screen.sh
@@ -8,10 +8,18 @@
 #
 # This script allows user to select a screen area, screenshots it,
 # OCRs it, and saves it into clipboard
+#
+# $1 : optional : language to use
+#   see $(tesseract --list-langs)
+
+tes_command="tesseract - -"
+if [ -n "$1" ]; then
+    tes_command="tesseract -l $1 - -"
+fi
 
 set -eu
 
-text=$(maim -s | tesseract - - | sed -E '/^\s*$/ d')
+text=$(maim -s | $tes_command | sed -E '/^\s*$/ d')
 if [ -z "$text" ]; then
     exit 1
 fi
