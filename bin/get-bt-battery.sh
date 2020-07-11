08339459e8144b290a4339c45934130e8cdc497c
Petr Šťastný <petr@soptik.tech>
2020-07-11 13:41:40 +0200

    Commented out unicode emojis because of poor font support

--- bin/get-bt-battery.sh
+++ bin/get-bt-battery.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # https://github.com/TheWeirdDev/Bluetooth_Headset_Battery_Level
-percentage=$(/home/petr/data/Applications/Bluetooth_Headset_Battery_Level/bl_battery.py 1C:52:16:CA:EE:DF.1 | grep -o '[0-9]*%')
+percentage=$(/home/petr/data/Applications/Bluetooth_Headset_Battery_Level/bluetooth_battery.py 1C:52:16:CA:EE:DF.1 | grep -o '[0-9]*%')
 
-echo ": $percentage"
+#echo ": $percentage"
+echo "BT: $percentage"
