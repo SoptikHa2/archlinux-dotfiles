d6ed4a7ecf8dd0ea3449ad382ffa3568175758b1
Petr Šťastný <petr@soptik.tech>
2020-07-13 21:46:21 +0200

    Fixed custom i3lock to properly pause notifications
    i3lock apparently forks to the background and we can't use
    shell `wait` to check if it quit yet. So we use pgrep the
    dumb way.

--- bin/lock-custom.sh
+++ bin/lock-custom.sh
@@ -9,6 +9,9 @@ trap 'unpause_notifications' EXIT
 # Pause notifications
 notify-send "DUNST_COMMAND_PAUSE"
 # i3 lock with custom image
-i3lock -i "/home/petr/archlinux-dotfiles/lockscreen.png" -ue &
-wait
+i3lock -i "/home/petr/archlinux-dotfiles/lockscreen.png" -ue
+
+while [[ $(pgrep 'i3lock' -c) -ge 1 ]]; do
+    sleep 5
+done
 # Notifications will be unpaused when this script exits
