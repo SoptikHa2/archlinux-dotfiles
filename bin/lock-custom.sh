4d0a3524131ddfe53ff102807b5e1a1493351e6a
Petr Šťastný <petr@soptik.tech>
2020-08-03 13:00:09 +0200

    Reimplemented lock to resize images

--- bin/lock-custom.sh
+++ bin/lock-custom.sh
@@ -9,7 +9,15 @@ trap 'unpause_notifications' EXIT
 # Pause notifications
 notify-send "DUNST_COMMAND_PAUSE"
 # i3 lock with custom image
-i3lock -i "/home/petr/archlinux-dotfiles/lockscreen.png" -ue
+# This takes ~/archlinux-dotfiles/lockscreen.png and resizes it to resolution detected by xrandr.
+# The file is saved into /tmp. If the resolution matches the already generated image, no new image
+# is produced and cached version is used instead.
+target_resolution="$(xrandr -q | grep -Eo '[0-9]+x[0-9]+' -m 1)"
+if [[ ! -f "/tmp/lockscreen-res.txt" ]] || [[ ! "$(grep -q -F "$target_resolution" "/tmp/lockscreen-res.txt")" ]]; then
+    convert -resize "$target_resolution" ~/archlinux-dotfiles/lockscreen.png /tmp/lockscreen-tmp.png
+    echo "$target_resolution" > /tmp/lockscreen-res.txt
+fi
+i3lock -i "/tmp/lockscreen-tmp.png" -ue
 
 while [[ $(pgrep 'i3lock' -c) -ge 1 ]]; do
     sleep 5
