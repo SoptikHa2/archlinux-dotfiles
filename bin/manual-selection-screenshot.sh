8b1b2c457bfd74d1d05b144e2a2cf71e1ad8f254
Petr Šťastný <petr@soptik.tech>
2020-08-03 10:48:57 +0200

    Added svg lockscreen and rendered 8k png

--- bin/manual-selection-screenshot.sh
+++ bin/manual-selection-screenshot.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -eu
+
 maim -us /tmp/"$USER"screenshot.png
 xclip -selection clipboard -t image/png </tmp/"$USER"screenshot.png
+chmod o-rw /tmp/"$USER"screenshot.png
 notify-send "Screenshot /tmp/""$USER""screenshot.png copied to clipboard"
