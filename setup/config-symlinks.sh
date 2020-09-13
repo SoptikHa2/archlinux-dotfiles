1506b92c0de2e3a665de70fb89a5d6fa183a58a3
Petr Šťastný <petr@soptik.tech>
2020-09-13 18:26:58 +0200

    Added new i3 files to symlink builder

--- setup/config-symlinks.sh
+++ setup/config-symlinks.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 
-# X & i3 symlinks
+# X
 ln -sf ~/archlinux-dotfiles/xinitrc ~/.xinitrc
-ln -sf ~/archlinux-dotfiles/i3/config ~/.config/i3
 for f in ~/archlinux-dotfiles/x/xorg.conf.d/*; do
 	echo "Linking $f -> /etc/X11/xorg.conf.d"
 	sudo ln -sf "$f" /etc/X11/xorg.conf.d
 done
+
+# i3
+mkdir -p ~/.config/i3
+ln -s ~/archlinux-dotfiles/i3/{config,i3_master.ini,i3-master-stack} ~/.config/i3
 
 # Sysctl
 ln -sf ~/archlinux-dotfiles/sysctl.d/*.conf /etc/sysctl.d
@@ -16,6 +19,7 @@ mkdir -p ~/.config/polybar
 ln -sf ~/archlinux-dotfiles/polybar/config ~/.config/polybar
 
 # Rofi
+mkdir -p ~/.config/rofi
 ln -sf ~/archlinux-dotfiles/rofi ~/.config/rofi
 
 # Alacritty
@@ -35,6 +39,7 @@ ln -sf ~/archlinux-dotfiles/zshrc ~/.zshrc
 ln -sf ~/archlinux-dotfiles/nvim ~/.config/nvim
 
 # SSH
+mkdir -p ~/.ssh
 ln -sf ~/archlinux-dotfiles/ssh-config ~/.ssh/config
 ln -sf ~/archlinux-dotfiles/pam_environment ~/.pam_environment
 
