cd0d6ffa60a4d26bf86fe83b26a8c4bbb1d252ae
Petr Šťastný <petr@soptik.tech>
2023-01-23 15:05:55 +0100

    Fixed lockscreen cache

--- bin/lock-custom.sh
+++ bin/lock-custom.sh
@@ -9,7 +9,7 @@ dunstctl set-paused true
 # The file is saved into /tmp. If the resolution matches the already generated image, no new image
 # is produced and cached version is used instead.
 target_resolution="$(xrandr -q | grep -Eo '[0-9]+x[0-9]+' -m 1)"
-if [[ ! -f "/tmp/lockscreen-res.txt" ]] || [[ ! "$(grep -q -F "$target_resolution" "/tmp/lockscreen-res.txt")" ]]; then
+if [[ ! -f "/tmp/lockscreen-res.txt" ]] || ! grep -q -F "$target_resolution" "/tmp/lockscreen-res.txt"; then
     convert -resize "$target_resolution" ~/archlinux-dotfiles/lockscreen.png /tmp/lockscreen-tmp.png
     echo "$target_resolution" > /tmp/lockscreen-res.txt
 fi
